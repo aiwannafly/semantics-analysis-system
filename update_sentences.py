a5a6dcdf4a7de5cbfc39e738ca2f5923861228cf
PasukkaBig <o_kristina2000@mail.ru>
2024-03-25 15:22:21 +0700

    relation Method_isPartOf_Method

--- update_sentences.py
+++ update_sentences.py
@@ -19,7 +19,7 @@ for sent in sentences:
         if rel.term1.class_ == 'Object':
             new_relations.append(Relation(rel.term2, 'isPartOf', rel.term1))
         elif rel.term1.class_ == 'Method':
-            new_relations.append(Relation(rel.term2, 'isComponentOf', rel.term1))
+            new_relations.append(Relation(rel.term2, 'isPartOf', rel.term1))
         else:
             new_relations.append(Relation(rel.term2, 'isExampleOf', rel.term1))
 

16ee6d02184834b46a64a0a3902b2de724d1630d
Aleksandr Ivanov <aivanov@dataeast.com>
2024-04-07 12:59:25 +0700

    Improve color for application

--- plot_graph.py
+++ plot_graph.py
@@ -10,7 +10,7 @@ from semantics_analysis.term_classification.roberta_term_classifier import LABEL
 
 colors = [
     '#f44336', '#e81e63', '#9c27b0', '#673ab7', '#3f51b5', '#2196f3', '#03a9f4', '#00bcd4', '#009688',
-    '#4caf50', '#8bc34a', '#cddc39', '#ffeb3b', '#ffc107', '#ff9800', '#ff5722'
+    '#4caf50', '#8bc34a', '#cddc39', '#ffc107', '#ff9800', '#ffeb3b', '#ff5722'
 ]
 
 

fdd2e101db4b77c1c195611a67374519e32d8a46
Aleksandr Ivanov <aivanov@dataeast.com>
2024-06-06 18:15:21 +0700

    Minor refactoring

--- semantics_analysis/llm_agent.py
+++ semantics_analysis/llm_agent.py
@@ -2,11 +2,10 @@ from typing import List, Optional
 
 from huggingface_hub import InferenceClient
 
-from semantics_analysis.tokens import tokens
+from semantics_analysis.values import values
 
 
 class LLMAgent:
-
     def __init__(
             self,
             model: str = 'mistralai/Mixtral-8x7B-Instruct-v0.1',
@@ -19,7 +18,7 @@ class LLMAgent:
         if huggingface_hub_token:
             token = huggingface_hub_token
         else:
-            token = tokens[self.token_idx]
+            token = values[self.token_idx]
 
         self.llm = InferenceClient(model=model, timeout=timeout, token=token)
         self.use_all_tokens = use_all_tokens
@@ -36,11 +35,11 @@ class LLMAgent:
                     max_new_tokens=max_new_tokens
                 ).strip()
             except Exception as e:
-                if attempt >= len(tokens) or not self.use_all_tokens:
+                if attempt >= len(values) or not self.use_all_tokens:
                     raise e
 
                 attempt += 1
                 self.token_idx += 1
-                self.token_idx = self.token_idx % len(tokens)
-                self.llm = InferenceClient(model=self.model, timeout=8, token=tokens[self.token_idx])
+                self.token_idx = self.token_idx % len(values)
+                self.llm = InferenceClient(model=self.model, timeout=8, token=values[self.token_idx])
                 continue
